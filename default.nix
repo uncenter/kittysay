439845cae4c0b956497f9ffbc6e7ab5be1c744f2
isabel <isabel@isabelroses.com>
2024-02-07 20:38:45 +0000

    chore: update version number

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 {pkgs ? import <nixpkgs> {}}:
 pkgs.rustPlatform.buildRustPackage {
   pname = "kittysay";
-  version = "0.2.0";
+  version = "0.3.0";
 
   src = ./.;
   cargoLock.lockFile = ./Cargo.lock;
