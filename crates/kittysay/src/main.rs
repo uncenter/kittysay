5864d49212d2793c2847f27f9349f210fd119e85
uncenter <uncenter@uncenter.dev>
2025-04-01 21:43:35 +0000

    fix(kittysay): remove custom colors option

--- crates/kittysay/src/main.rs
+++ crates/kittysay/src/main.rs
@@ -3,10 +3,7 @@ use color_eyre::eyre::Result;
 use clap::Parser;
 use clap_stdin::MaybeStdin;
 
-use anstream::{adapter::strip_str, println};
 use crossterm::terminal;
-
-use kittycore::{generate, FormatOptions, KITTY};
 
 #[derive(Parser)]
 #[command(version, about)]
@@ -19,9 +16,6 @@ struct Cli {
 	/// Enable kittythink mode (thought bubble)
 	#[clap(long, short)]
 	think: bool,
-	/// Use custom colors. The first colors the message, the second colors the cat
-	#[arg(long, short, num_args(2))]
-	colors: Option<Vec<u8>>,
 }
 
 fn main() -> Result<()> {
@@ -32,25 +26,12 @@ fn main() -> Result<()> {
 
 	let width = args.width.unwrap_or(45).min(cols.saturating_sub(5));
 
-	let format_opts = FormatOptions {
+	let format_opts = kittycore::FormatOptions {
 		think: args.think,
 		width,
 	};
 
-	let msg = generate(&strip_str(&args.message).to_string(), &format_opts);
-
-	let mut msg_color = console::Color::White;
-	let mut cat_color = console::Color::White;
-	if let Some(colors) = args.colors {
-		msg_color = console::Color::Color256(colors[0]);
-		cat_color = console::Color::Color256(colors[1]);
-	}
-
-	println!(
-		"{}{}",
-		console::style(msg).fg(msg_color),
-		console::style(KITTY).fg(cat_color)
-	);
+	println!("{}", kittycore::print(&args.message, &format_opts));
 
 	Ok(())
 }
