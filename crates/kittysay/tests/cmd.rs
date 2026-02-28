55c5da26a85ba61aa53306d8ad102d9589a4b414
uncenter <uncenter@uncenter.dev>
2026-02-27 18:13:11 -0600

    test(kittysay): update snapshots

--- crates/kittysay/tests/cmd.rs
+++ crates/kittysay/tests/cmd.rs
@@ -22,6 +22,7 @@ fn test_help() {
 	  -w, --width <WIDTH>             Set width of speech/thought bubble
 	  -t, --think                     Enable kittythink mode (thought bubble)
 	  -c, --colors <COLORS> <COLORS>  Use custom colors. The first colors the message, the second colors the cat
+	      --tab-size <TAB_SIZE>       Set the interpreted width/length of tabs [default: 4]
 	  -h, --help                      Print help
 	  -V, --version                   Print version
 
@@ -86,7 +87,7 @@ them puke.
 	/ What is comedy?  Comedy is the art of making \
 	| people laugh without making                  |
 	| them puke.                                   |
-	\ 		-- Steve Martin                            /
+	\         -- Steve Martin                      /
 	  --------------------------------------------
 	  \
 	    \
@@ -104,11 +105,11 @@ them puke.
 	exit_code: 0
 	----- stdout -----
 
-	  ------------------------------------------
-	/ Grabel's Law:                              \
-	| 	2 is not equal to 3 -- not even for large |
-	\ values of 2.                               /
-	  ------------------------------------------
+	  ---------------------------------------------
+	/ Grabel's Law:                                 \
+	|     2 is not equal to 3 -- not even for large |
+	\ values of 2.                                  /
+	  ---------------------------------------------
 	  \
 	    \
 	      ／l、
